8ee3e5281939172cdfdccae4589245f297459430
Ahmad Abdulhai <ahmad52585258@gmail.com>
2025-04-15 23:49:40 +0300

    Adding C application projects example for embedded C using "minGW" toolschain

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+* SourceFile main.C
+* Description:
+*  Created on: April , 16, 2025
+*  Author: Ahmad Abdulhai @ Hexabitz
+
+******************************************************************************
+* @attention
+*
+* Copyright (c) 2025 Hexabitz.
+* All rights reserved.
+*
+******************************************************************************
+*/
+#include <stdio.h>
+int main() {
+
+/*Print string on console*/
+    printf("Hello world\n");
+    return 0;
+}
