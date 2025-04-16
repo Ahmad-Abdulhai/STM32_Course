271e174139b9fab01308acffc6d1ec2e2bd58431
Ahmad Abdulhai <ahmad52585258@gmail.com>
2025-04-17 00:23:34 +0300

    Adding Example baisc of Operators on variable

--- main.c
+++ main.c
@@ -1,47 +1,83 @@
 /*
-* SourceFile main.C
-* Description:
-*  Created on: April , 16, 2025
-*  Author: Ahmad Abdulhai @ Hexabitz
+ * SourceFile main.C
+ * Description:
+ *  Created on: April , 16, 2025
+ *  Author: Ahmad Abdulhai @ Hexabitz
 
-******************************************************************************
-* @attention
-*
-* Copyright (c) 2025 Hexabitz.
-* All rights reserved.
-*
-******************************************************************************
-*/
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 Hexabitz.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 int main() {
 
-		/* 1 - Casting float to int
-		 *  Gives you a floating-point number to an integer, truncating any decimal places
-		 *  */
-		float a = 5.75423;
-		int32_t b = (int) a;
-		printf("Result: %d\n\n", b);
-/******************************************************************************/
-
-		/*2 - Casting char to int*/
-		char ch = 'A';
-		int ascii_value = (int)ch;
-		printf("ASCII Value of %c: %d\n\n", ch, ascii_value);
-/******************************************************************************/
-		/* 3 - Casting uint32_t to uint8_t
-		 *  Gives you the least significant byte of the uint32_t value.
-		 * */
-		uint32_t largeValue = 0x12345678;
-		uint8_t smallValue = (uint8_t)(largeValue); // smallValue will be 0x78
-		printf("smallValue after casting: 0x%x\n\n", smallValue);
+	   /*Arithmetic Operators*/
+//     int a = 10, b = 3;
+//	    printf("Arithmetic Operators:\n");
+//	    printf("Addition: %d + %d = %d\n", a, b, a + b);           // Addition
+//	    printf("Subtraction: %d - %d = %d\n", a, b, a - b);        // Subtraction
+//	    printf("Multiplication: %d * %d = %d\n", a, b, a * b);     // Multiplication
+//	    printf("Division: %d / %d = %d\n", a, b, a / b);           // Division
+//	    printf("Modulus: %d %% %d = %d\n", a, b, a % b);         // Modulus
+//	    float f = (float)a/b;
+//	    printf("Division after casting: %d / %d = %f\n\n", a, b, f); // Division with casting
 
 /******************************************************************************/
-		/* 4 - Casting double to float */
-		double highPrecisionValue = 123.456789012345;
-		float lessPrecisionValue = (float)(highPrecisionValue); // lessPrecisionValue will be approximately 123.45679
-		printf("lessPrecisionValue after casting:%f\n\n", lessPrecisionValue);
+	   /*Relational Operators*/
+//	   printf("Relational Operators:\n");
+//	   printf("Equal: %d == %d = %d\n", a, b, a == b);            // Equal to
+//	   printf("Not Equal: %d != %d = %d\n", a, b, a != b);        // Not equal to
+//	   printf("Greater Than: %d > %d = %d\n", a, b, a > b);       // Greater than
+//	   printf("Less Than: %d < %d = %d\n", a, b, a < b);          // Less than
+//	   printf("Greater or Equal: %d >= %d = %d\n", a, b, a >= b); // Greater than or equal to
+//	   printf("Less or Equal: %d <= %d = %d\n\n", a, b, a <= b); // Less than or equal to
 
-    return 0;
+/******************************************************************************/
+	/*Logical Operators*/
+//	   bool x = 1, y = 0;
+//	   printf("Logical Operators:\n");
+//	   printf("Logical AND: %d && %d = %d\n", x, y, x && y);      // Logical AND
+//	   printf("Logical OR: %d || %d = %d\n", x, y, x || y);       // Logical OR
+//	   printf("Logical NOT: !%d = %d\n\n", x, !x);                // Logical NOT
+
+/******************************************************************************/
+
+	   /* Assignment Operators*/
+	   int c = 10;
+	   printf("Assignment Operators:\n");
+	   c += 5;  // Equivalent to c = c + 5
+	   printf("Addition Assignment: c += 5 -> c = %d\n", c);
+
+	   c -= 3;  // Equivalent to c = c - 3
+	   printf("Subtraction Assignment: c -= 3 -> c = %d\n", c);
+
+	   c *= 2;  // Equivalent to c = c * 2
+	   printf("Multiplication Assignment: c *= 2 -> c = %d\n", c);
+
+	   c /= 4;  // Equivalent to c = c / 4
+	   printf("Division Assignment: c /= 4 -> c = %d\n", c);
+
+	   c %= 3;  // Equivalent to c = c % 3
+	   printf("Modulus Assignment: c %%= 3 -> c = %d\n\n", c);
+
+/******************************************************************************/
+	   /*Increment and Decrement Operators*/
+	   int d = 5;
+	   printf("Increment and Decrement Operators:\n");
+	   printf("Original Value: %d\n", d);
+
+	   d++;  // Increment
+	   printf("After Increment: %d\n", d);
+
+	   d--;  // Decrement
+	   printf("After Decrement: %d\n\n", d);
+
+	return 0;
 }
