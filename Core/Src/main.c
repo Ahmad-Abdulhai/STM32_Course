bbe6a0c1ad4444dbfc4978e1199ed00d9a9dd4ba
Ahmad Abdulhai <@Aa52585258aA@>
2025-05-18 15:16:10 +0300

    Adding main code for Exercise3 "Create multiple tasks with different priorities, observe their execution, and use os{ThreadResum},{osThreadSuspend},{osThreadTerminate} for controlled execution"

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -44,9 +44,8 @@ UART_HandleTypeDef huart2;
 
 osThreadId defaultTaskHandle;
 osThreadId Task2Handle;
-osThreadId Task3Handle;
 /* USER CODE BEGIN PV */
-
+uint32_t indx = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -55,13 +54,12 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 void StartDefaultTask(void const * argument);
 void StartTask2(void const * argument);
-void StartTask3(void const * argument);
 
 /* USER CODE BEGIN PFP */
 /*To make printf() work over UART in STM32, we override(Redirected to UART)*/
 int _write(int file, char *ptr, int len) {
-    HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 100);
-    return len;
+	HAL_UART_Transmit(&huart2, (uint8_t*) ptr, len, 100);
+	return len;
 }
 /* USER CODE END PFP */
 
@@ -104,19 +102,19 @@ int main(void)
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
+	/* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
+	/* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
+	/* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
   /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
+	/* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
@@ -125,15 +123,11 @@ int main(void)
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* definition and creation of Task2 */
-  osThreadDef(Task2, StartTask2, osPriorityBelowNormal, 0, 128);
+  osThreadDef(Task2, StartTask2, osPriorityAboveNormal, 0, 128);
   Task2Handle = osThreadCreate(osThread(Task2), NULL);
 
-  /* definition and creation of Task3 */
-  osThreadDef(Task3, StartTask3, osPriorityAboveNormal, 0, 128);
-  Task3Handle = osThreadCreate(osThread(Task3), NULL);
-
   /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
+	/* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
@@ -142,12 +136,11 @@ int main(void)
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+	}
   /* USER CODE END 3 */
 }
 
@@ -259,61 +252,60 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-	  /*Toggle LED on bard*/
-	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-	  printf("\rHello DEFTASK\n");
-     osDelay(500);
-  }
+	/* Infinite loop */
+	for (;;) {
+		/*Toggle LED on bard*/
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+		printf("\rHello DEFTASK\n");
+		osDelay(1000);
+	}
   /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_StartTask2 */
 /**
-* @brief Function implementing the Task2 thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task2 thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartTask2 */
 void StartTask2(void const * argument)
 {
   /* USER CODE BEGIN StartTask2 */
-  /* Infinite loop */
-  for(;;)
-  {
-	  printf("\rHello TASK2\n");
-    osDelay(500);
-  }
-  /* USER CODE END StartTask2 */
-}
+	/* Infinite loop */
+	for (;;) {
+		printf("\rTASK2, indx= %lu\n", indx++);
+		/*Suspend default task*/
+		if (indx == 4) {
+			printf("\rSuspending DefaultTask\n");
+			osThreadSuspend(defaultTaskHandle);
+		}
+		/*Resume default task*/
+		if (indx == 7) {
+			printf("\rResuming DefaultTask\n");
+			osThreadResume(defaultTaskHandle);
+		}
+		/*terminate default task*/
+		if (indx == 10){
+			printf ("\rTerminating DefaultTask\n");
+			osThreadTerminate(defaultTaskHandle);
+		}
 
-/* USER CODE BEGIN Header_StartTask3 */
-/**
-* @brief Function implementing the Task3 thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartTask3 */
-void StartTask3(void const * argument)
-{
-  /* USER CODE BEGIN StartTask3 */
-  /* Infinite loop */
-  for(;;)
-  {
-	  printf("\rHello TASK3\n");
-    osDelay(500);
-  }
-  /* USER CODE END StartTask3 */
+//		if (indx == 10){
+//			uint32_t PreviousWakeTime = osKernelSysTick();
+//			osDelayUntil(&PreviousWakeTime, 5000);  // 3 sec inactive
+//		}
+		osDelay(2000);
+	}
+  /* USER CODE END StartTask2 */
 }
 
 /**
@@ -344,11 +336,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 
